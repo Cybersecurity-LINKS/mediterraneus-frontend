2f23b05e7b2ba7317d283f33f257727a5075be58
Luca Giorgino <19236900+lucagiorgino@users.noreply.github.com>
2024-02-13 15:45:10 +0100

    Update index.tsx

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,18 +1,9 @@
-import { AbiCoder, BigNumberish, InterfaceAbi, ethers, keccak256, solidityPacked, toUtf8Bytes } from 'ethers'
+import { InterfaceAbi, ethers } from 'ethers'
+// import { AbiCoder, BigNumberish, keccak256, solidityPacked, toUtf8Bytes } from 'ethers'
 import { Credential } from "@iota/identity-wasm/web";
 import contractAddresses from '../addresses/contractAddresses.json'
 
 const EVM_EXPLORER = import.meta.env.VITE_EVM_EXPLORER as string;
-
-const enum CONTRACT_ADRRESS {
-  "Deployer" = 0,
-  "ERC721Base" = 1,
-  "ERC20Base" = 2,
-  "RouterFactory" = 3,
-  "FixedRateExchange" = 4,
-  "IDentity" = 5,
-  "ERC721Factory" = 6,
-}
 
 export const NETWORKS: { [k: string]: string } = {
   1: "Ethereum Main Network",
@@ -94,25 +85,25 @@ export const getContractAddress = (contractName: string) => {
     let ret;
     switch (contractName) {
       case "Deployer":
-        ret = contractAddresses.addresses[CONTRACT_ADRRESS.Deployer].Deployer?.toString()
+        ret = contractAddresses.addresses.Deployer?.toString()
         break;
       case "ERC721Base":
-        ret = contractAddresses.addresses[CONTRACT_ADRRESS.ERC721Base].ERC721Base?.toString()
+        ret = contractAddresses.addresses.ERC721Base?.toString()
         break;
       case "ERC20Base":
-        ret = contractAddresses.addresses[CONTRACT_ADRRESS.ERC20Base].ERC20Base?.toString()
+        ret = contractAddresses.addresses.ERC20Base?.toString()
         break;
       case "ERC721Factory":
-        ret = contractAddresses.addresses[CONTRACT_ADRRESS.ERC721Factory].ERC721Factory?.toString()
+        ret = contractAddresses.addresses.ERC721Factory?.toString()
         break;
       case "RouterFactory":
-        ret = contractAddresses.addresses[CONTRACT_ADRRESS.RouterFactory].RouterFactory?.toString()
+        ret = contractAddresses.addresses.RouterFactory?.toString()
         break;
       case "FixedRateExchange":
-        ret = contractAddresses.addresses[CONTRACT_ADRRESS.FixedRateExchange].FixedRateExchange?.toString()
+        ret = contractAddresses.addresses.FixedRateExchange?.toString()
         break;
       case "IDentity":
-          ret = contractAddresses.addresses[CONTRACT_ADRRESS.IDentity].IDentity?.toString()
+          ret = contractAddresses.addresses.IDentityAddress?.toString()
           break;
       default:
         ret = "";
@@ -123,80 +114,80 @@ export const getContractAddress = (contractName: string) => {
   }
 }
 
-export const PERMIT_TYPEHASH = keccak256(
-  toUtf8Bytes('Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)')
-)
+// export const PERMIT_TYPEHASH = keccak256(
+//   toUtf8Bytes('Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)')
+// )
 
-// Returns the EIP712 hash which should be signed by the user
-// in order to make a call to `permit`
-export function getPermitDigest(
-  name: string,
-  address: string,
-  chainId: number,
-  approve: {
-    owner: string
-    spender: string
-    value: BigNumberish
-  },
-  nonce: BigNumberish,
-  deadline: BigNumberish
-) {
-  const DOMAIN_SEPARATOR = getDomainSeparator(name, address, chainId)
-  return keccak256(
-    solidityPacked(
-      ['bytes1', 'bytes1', 'bytes32', 'bytes32'],
-      [
-        '0x19',
-        '0x01',
-        DOMAIN_SEPARATOR,
-        keccak256(
-          new AbiCoder().encode(
-            ['bytes32', 'address', 'address', 'uint256', 'uint256', 'uint256'],
-            [PERMIT_TYPEHASH, approve.owner, approve.spender, approve.value, nonce, deadline]
-          )
-        ),
-      ]
-    )
-  )
-}
+// // Returns the EIP712 hash which should be signed by the user
+// // in order to make a call to `permit`
+// export function getPermitDigest(
+//   name: string,
+//   address: string,
+//   chainId: number,
+//   approve: {
+//     owner: string
+//     spender: string
+//     value: BigNumberish
+//   },
+//   nonce: BigNumberish,
+//   deadline: BigNumberish
+// ) {
+//   const DOMAIN_SEPARATOR = getDomainSeparator(name, address, chainId)
+//   return keccak256(
+//     solidityPacked(
+//       ['bytes1', 'bytes1', 'bytes32', 'bytes32'],
+//       [
+//         '0x19',
+//         '0x01',
+//         DOMAIN_SEPARATOR,
+//         keccak256(
+//           new AbiCoder().encode(
+//             ['bytes32', 'address', 'address', 'uint256', 'uint256', 'uint256'],
+//             [PERMIT_TYPEHASH, approve.owner, approve.spender, approve.value, nonce, deadline]
+//           )
+//         ),
+//       ]
+//     )
+//   )
+// }
 
-// Gets the EIP712 domain separator
-export function getDomainSeparator(name: string, contractAddress: string, chainId: number) {
-  const h1 = keccak256(toUtf8Bytes('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'));
-  const h2 = keccak256(toUtf8Bytes(name));
-  const h3 = keccak256(toUtf8Bytes('1'));
+// // Gets the EIP712 domain separator
+// export function getDomainSeparator(name: string, contractAddress: string, chainId: number) {
+//   const h1 = keccak256(toUtf8Bytes('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'));
+//   const h2 = keccak256(toUtf8Bytes(name));
+//   const h3 = keccak256(toUtf8Bytes('1'));
 
-  console.log(h1, h2, h3, chainId, contractAddress);
-  return keccak256(
-    new AbiCoder().encode(
-      ['bytes32', 'bytes32', 'bytes32', 'uint256', 'address'],
-      [
-        h1,
-        h2,
-        h3,
-        chainId,
-        contractAddress,
-      ]
-    )
-  )
-}
+//   console.log(h1, h2, h3, chainId, contractAddress);
+//   return keccak256(
+//     new AbiCoder().encode(
+//       ['bytes32', 'bytes32', 'bytes32', 'uint256', 'address'],
+//       [
+//         h1,
+//         h2,
+//         h3,
+//         chainId,
+//         contractAddress,
+//       ]
+//     )
+//   )
+// }
 
-export function getSemiPermitDigest(
-  approve: {
-    owner: string
-    spender: string
-    value: BigNumberish
-  },
-  nonce: BigNumberish,
-  deadline: BigNumberish
-) {
-  return keccak256(
-    new AbiCoder().encode(
-      ['bytes32', 'address', 'address', 'uint256', 'uint256', 'uint256'],
-      [PERMIT_TYPEHASH, approve.owner, approve.spender, approve.value, nonce, deadline]
-    )
-  )
-}
+// export function getSemiPermitDigest(
+//   approve: {
+//     owner: string
+//     spender: string
+//     value: BigNumberish
+//   },
+//   nonce: BigNumberish,
+//   deadline: BigNumberish
+// ) {
+//   return keccak256(
+//     new AbiCoder().encode(
+//       ['bytes32', 'address', 'address', 'uint256', 'uint256', 'uint256'],
+//       [PERMIT_TYPEHASH, approve.owner, approve.spender, approve.value, nonce, deadline]
+//     )
+//   )
+// }
 
 export const privKeytoBytes = (text: string): Uint8Array => {
   const buffer = text.split(",");
